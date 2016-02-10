254396ab2fb5c2cfc77a55b0935b682e5b99a46e
Brioal <821329382@qq.com>
2016-02-10 18:43:01 +0800

    实现录音的功能

--- app/src/main/java/com/brioal/mediarecodertest/MainActivity.java
+++ app/src/main/java/com/brioal/mediarecodertest/MainActivity.java
@@ -1,14 +1,22 @@
 package com.brioal.mediarecodertest;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+//TODO 笔记 知识点
 public class MainActivity extends AppCompatActivity {
-    
+    private File file ;
+    private MediaRecorder mRecorder ;
+    private Button btn_play ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,48 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        btn_play = (Button) findViewById(R.id.main_btn_start);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (file != null && file.exists()) {
+            mRecorder.stop();
+            //释放资源
+            mRecorder.release();
+            mRecorder = null;
+        }
+        super.onDestroy();
     }
 
     public void record(View view) {
+        if (file == null) {
+            btn_play.setText("Stop");
+            File extral = Environment.getExternalStorageDirectory();
+            try {
+                file = new File(extral.getCanonicalPath() + "/sound.amr");
+                mRecorder = new MediaRecorder();
+                //设置声音来源
+                mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                //设置声音输出格式 必须在设置声音编码格式之前设置
+                mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                //设置声音编码格式
+                mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+                //设置输出目录
+                mRecorder.setOutputFile(file.getAbsolutePath());
+                mRecorder.prepare();
+                mRecorder.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            btn_play.setText("Start");
+            mRecorder.stop();
+            mRecorder.release();
+        }
+
 
     }
 }
